3f07e544149cb36a497091d19c51dba91595bd9e
tajbaba999 <tajbaba153@gmail.com>
2023-12-21 19:45:13 +0530

    keyword

---
+++ keyword.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char kw[32][10] = {
+        "int",
+        "char",
+        "float",
+        "double",
+    };
+    printf("Enter the string :");
+    char str[25];
+    int cnt = 0, flag;
+    scanf("%s", &str);
+    for (int i = 0; i < 32; i++)
+    {
+        if (strcmp(str) == 0)
+        {
+            flag = 1;
+        }
+    }
+    if (flag == 1)
+    {
+        printf("%s is a keyword", str);
+    }
+    else
+    {
+        printf(" %s is not a keyword\n", str);
+    }
+    return 0;
+}
