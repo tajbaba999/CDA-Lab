12bada6f785819109bac71fa09baf1bc971149d0
tajbaba999 <tajbaba153@gmail.com>
2024-01-05 19:49:43 +0530

    bfs

--- bfs.c
+++ bfs.c
@@ -1,36 +1,36 @@
 #include <stdio.h>
 
 int queue[100] = {0}, front = 1, rear = 1;
-int visited[10];
+int isVisited[10];
 int count = 1;
 
-void bfs(int g[5][5], int v, int u)
+bfs(int graph[5][5], int v, int u)
 {
-    for (int i = 0; i < v; i++)
-    {
-        if (visited[i] == 0 && g[u][i] == 1)
-        {
-            visited[i] = 1;
-            queue[rear++] = i;
-            count++;
-        }
-    }
-
     if (count == v)
         return;
 
-    bfs(g, v, queue[front++]);
+    for (int i = 0; i < v; i++)
+    {
+        if (isVisited[i] == 0 && graph[u][i] == 1)
+        {
+            isVisited[i] = 1;
+            count++;
+            queue[rear++] = i;
+        }
+    }
+    bfs(graph, v, queue[front++]);
 }
 
-int main()
+void main()
 {
-    int a[5][5] = {{0, 1, 0, 0, 0}, {1, 0, 0, 1, 0}, {0, 0, 0, 0, 1}, {0, 1, 0, 0, 1}, {0, 0, 1, 1, 0}};
+    // printf("Enter the input :");
+    // scanf("%d",&)
+    int graph[5][5] = {{0, 1, 1, 0, 0}, {1, 0, 1, 1, 0}, {1, 1, 0, 0, 1}, {0, 1, 0, 0, 1}, {0, 0, 1, 1, 0}};
     int v = 5;
-    visited[0] = 1;
-    bfs(a, v, 0);
+    isVisited[0] = 1;
+    bfs(graph, v, 0);
     for (int i = 0; i < v; i++)
     {
         printf("%d ", queue[i]);
     }
-    printf("\n");
 }
